3e632d2e4e9f849d35fdff3271a1acb7c30f7b9a
Yuga Samuel <yugasamuel2002@gmail.com>
2023-12-29 12:10:45 +0700

    Initial Commit

---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Yuga Samuel on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
